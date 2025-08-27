67a801513db557f7c667660d4248b430d0521ffd
NaviCo-de <aridarrell30@gmail.com>
2025-08-27 15:50:24 +0700

    Test

--- football_news/football_news/settings.py
+++ football_news/football_news/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#Test
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
