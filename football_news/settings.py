d47dd3005100135cfc28077adf53a58eab262119
NaviCo-de <aridarrell30@gmail.com>
2025-09-03 15:21:19 +0700

    Buat folder main, add template, make routing, add views, add unit test

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ari-darrell-footballnews.pbp.cs.ui.a
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
