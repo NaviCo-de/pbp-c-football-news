488b01cb21e02dcdde6e2c59c08032ced7fd9622
NaviCo-de <aridarrell30@gmail.com>
2025-09-24 16:19:34 +0700

    Tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout')
+    path('logout/', logout_user, name='logout'),
+    path('news/<uuid:id>/edit', edit_news, name='edit_news'),
+    path('news/<uuid:id>/delete', delete_news, name='delete_news'),
 ]
